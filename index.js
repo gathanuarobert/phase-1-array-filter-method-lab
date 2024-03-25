2ae7f1ab08e5c271dc9fcbbba32b15a52e28df09
Robert Gathanua <robertgathanua@gamail.com>
2024-03-25 15:19:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(driver, query){
+    return driver.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+}
+  
+function fuzzyMatch(driver, query){
+    const matchingDrivers = driver.filter(driver =>
+        driver.toLowerCase().startsWith(query.toLowerCase())
+    );
+
+    return matchingDrivers;
+
+}
+    
+function matchName(driver, query){
+    const matchingDrivers = driver.filter(drivers =>
+        drivers.name.toLowerCase() === query.toLowerCase()
+    );
+
+    return matchingDrivers;
+}  
